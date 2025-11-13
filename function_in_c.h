6e88c757f39cea2308643c785697c7ded5d920a3
pravin-more <pravinmoreone@gmail.com>
2025-11-13 19:33:03 +0530

    C variable

---
+++ function_in_c.h
@@ -0,0 +1,16 @@
+//
+// Created by PravinMore on 13/11/2025.
+//
+
+#ifndef CODEREF_CLANG_FUNCTION_IN_C_H
+#define CODEREF_CLANG_FUNCTION_IN_C_H
+
+
+class function_in_c {
+
+
+
+};
+
+
+#endif //CODEREF_CLANG_FUNCTION_IN_C_H
